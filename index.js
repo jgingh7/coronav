ef5b8ced02d4c83bc147bf376236c41d3e3a5bf3
Jin Gyu Lee <ccjglingm07@gmail.com>
2021-01-11 20:49:58 -0500

    update 1/11 status

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-google.charts.load('current', {packages: ['corechart', 'line']});
+google.charts.load('current', {
+  packages: ['corechart', 'line']
+});
 google.charts.setOnLoadCallback(drawTrendlinesNewYork);
 google.charts.setOnLoadCallback(drawTrendlinesNewark);
 google.charts.setOnLoadCallback(drawTrendlinesDane);
@@ -7,12 +9,12 @@ google.charts.setOnLoadCallback(drawTrendlinesPhilly);
 google.charts.setOnLoadCallback(drawTrendlinesLA);
 
 function drawTrendlinesNewYork() {
-  const numbers = [2512,
+  const numbers = [
     4029, 3366, 3851,
     4800, 5041, 2937,
     2892, 3956, 3969,
     5077, 5005, 5006,
-    5045
+    5045, 4306
   ]
 
   const averages = [0, 0]
@@ -21,12 +23,12 @@ function drawTrendlinesNewYork() {
 }
 
 function drawTrendlinesNewark() {
-  const numbers = [109,
+  const numbers = [
     100, 152, 199,
     228, 166, 130,
     56, 111, 253,
     280, 197, 298,
-    204
+    204, 101
   ]
 
   const averages = [0, 0]
@@ -35,12 +37,12 @@ function drawTrendlinesNewark() {
 }
 
 function drawTrendlinesDane() {
-  const numbers = [167,
+  const numbers = [
     71, 123, 156,
     156, 157, 270,
     205, 176, 190,
     753, 293, 190,
-    191
+    191, 134
   ]
 
   const averages = [0, 0]
@@ -49,12 +51,12 @@ function drawTrendlinesDane() {
 }
 
 function drawTrendlinesChicago() {
-  const numbers = [749,
+  const numbers = [
     691, 1262, 1307,
     949, 949, 1008,
     990, 1250, 1037,
     1590, 1268, 1269,
-    909
+    909, 988
   ]
 
   const averages = [0, 0]
@@ -63,11 +65,13 @@ function drawTrendlinesChicago() {
 }
 
 function drawTrendlinesPhilly() {
-  const numbers = [386,
+  const numbers = [
     925, 352, 1179,
     404, 404, 405,
     405, 916, 680,
-    792, 650]
+    792, 650, 517,
+    517, 517
+  ]
 
   const averages = [0, 0]
   getAvg(numbers, averages, 1584000)
@@ -75,12 +79,12 @@ function drawTrendlinesPhilly() {
 }
 
 function drawTrendlinesLA() {
-  const numbers = [5276,
+  const numbers = [
     4607, 4202, 5863,
     8354, 5625, 5625,
     3780, 4378, 4743,
     8374, 7458, 6885,
-    5154
+    5154, 5242
   ]
 
   const averages = [0, 0]
@@ -91,7 +95,7 @@ function drawTrendlinesLA() {
 
 function getAvg(numbers, averages, totalPop) {
   let summation = 0
-  for (var i=0; i<numbers.length; i++) {
+  for (var i = 0; i < numbers.length; i++) {
     summation += numbers[i]
   }
   averages[0] = Math.round(summation / numbers.length)
@@ -99,65 +103,69 @@ function getAvg(numbers, averages, totalPop) {
 }
 
 function getChart(city, numbers, avgNum, avgPercentage, chartName, dates = [
-  new Date('Dec 28, 2020'),
-  new Date('Dec 29, 2020'), new Date('Dec 30, 2020'), new Date('Dec 31, 2020'),
-  new Date('Jan 1, 2021'), new Date('Jan 2, 2021'), new Date('Jan 3, 2021'),
-  new Date('Jan 4, 2021'), new Date('Jan 5, 2021'), new Date('Jan 6, 2021'),
-  new Date('Jan 7, 2021'), new Date('Jan 8, 2021'), new Date('Jan 9, 2021'),
-  new Date('Jan 10, 2021')]) {
+    new Date('Dec 29, 2020'), new Date('Dec 30, 2020'), new Date('Dec 31, 2020'),
+    new Date('Jan 1, 2021'), new Date('Jan 2, 2021'), new Date('Jan 3, 2021'),
+    new Date('Jan 4, 2021'), new Date('Jan 5, 2021'), new Date('Jan 6, 2021'),
+    new Date('Jan 7, 2021'), new Date('Jan 8, 2021'), new Date('Jan 9, 2021'),
+    new Date('Jan 10, 2021'), new Date('Jan 11, 2021')]) {
 
-  var data = new google.visualization.DataTable();
-  data.addColumn('date', 'Date');
-  data.addColumn('number', 'Daily Positive People');
+    var data = new google.visualization.DataTable();
+    data.addColumn('date', 'Date');
+    data.addColumn('number', 'Daily Positive People');
 
-  data.addRows([
-    [dates[0], numbers[0]],
-    [dates[1], numbers[1]],
-    [dates[2], numbers[2]],
-    [dates[3], numbers[3]],
-    [dates[4], numbers[4]],
-    [dates[5], numbers[5]],
-    [dates[6], numbers[6]],
-    [dates[7], numbers[7]],
-    [dates[8], numbers[8]],
-    [dates[9], numbers[9]],
-    [dates[10], numbers[10]],
-    [dates[11], numbers[11]],
-    [dates[12], numbers[12]],
-    [dates[13], numbers[13]],
-    [dates[14], numbers[14]]
-  ]);
+    data.addRows([
+      [dates[0], numbers[0]],
+      [dates[1], numbers[1]],
+      [dates[2], numbers[2]],
+      [dates[3], numbers[3]],
+      [dates[4], numbers[4]],
+      [dates[5], numbers[5]],
+      [dates[6], numbers[6]],
+      [dates[7], numbers[7]],
+      [dates[8], numbers[8]],
+      [dates[9], numbers[9]],
+      [dates[10], numbers[10]],
+      [dates[11], numbers[11]],
+      [dates[12], numbers[12]],
+      [dates[13], numbers[13]],
+      [dates[14], numbers[14]]
+    ]);
 
-  var view = new google.visualization.DataView(data);
-  view.setColumns([0, 1, {
+    var view = new google.visualization.DataView(data);
+    view.setColumns([0, 1, {
       calc: 'stringify',
       sourceColumn: 1,
       type: 'string',
       role: 'annotation'
-  }]);
+    }]);
 
-  const title = city + ' Daily Positive (' + avgNum + ', ' + avgPercentage + '%)'
+    const title = city + ' Daily Positive (' + avgNum + ', ' + avgPercentage + '%)'
 
-  var options = {
-    title: title,
-    legend: 'none',
-    hAxis: {
-      title: 'Date'
-    },
-    vAxis: {
-      title: 'Number of People'
-    },
-    colors: ['#AB0D06'],
-    trendlines: {
-      0: {
-        type: 'linear',
-        color: '#111',
-        opacity: .3
+    var options = {
+      title: title,
+      legend: 'none',
+      hAxis: {
+        title: 'Date'
+      },
+      vAxis: {
+        title: 'Number of People'
+      },
+      colors: ['#AB0D06'],
+      trendlines: {
+        0: {
+          type: 'linear',
+          color: '#111',
+          opacity: .3
+        }
+      },
+      chartArea: {
+        left: 100,
+        top: 100,
+        width: "68%",
+        height: "65%"
       }
-    },
-    chartArea:{left:100,top:100,width:"68%",height:"65%"}
-  };
+    };
 
-  var chart = new google.visualization.LineChart(document.getElementById(chartName));
-  chart.draw(view, options);
-}
+    var chart = new google.visualization.LineChart(document.getElementById(chartName));
+    chart.draw(view, options);
+  }
